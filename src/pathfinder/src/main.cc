241aa7e645cd2a2545b003104de1ac92b8bdfb09
Theo Butler <theodusbutler@gmail.com>
2021-05-16 17:00:45 -0400

    Add some documentation to the code

--- src/pathfinder/src/main.cc
+++ src/pathfinder/src/main.cc
@@ -5,8 +5,10 @@
 #include <ros/ros.h>
 #include <sensor_msgs/LaserScan.h>
 #include <std_msgs/UInt16.h>
+#include <utility>
 #include <vector>
 
+/// `/mcu/ctl` message
 struct CtlMsg
 {
   int8_t throttle;
@@ -30,6 +32,8 @@ class Pathfinder
   static constexpr float rad_to_deg = 180.0 / M_PI;
   static constexpr float deg_to_rad = M_PI / 180.0;
   static constexpr float angle_max = 60.0 * deg_to_rad;
+  static constexpr float stop_threshold_ahead = 1.0;
+  static constexpr float stop_threshold_around = 0.5;
 
   std::vector<float> _angles;
   std::vector<float> _filtered;
@@ -37,49 +41,20 @@ class Pathfinder
   int8_t _steering_angle = 0;
 
 public:
-  Pathfinder() {}
-
+  /// Compute a `CtlMsg` for the MCU based on the given frame from the LiDAR.
   CtlMsg update_frame(sensor_msgs::LaserScan const& frame)
   {
     if (_angles.size() == 0)
+      _init_buffers(frame);
+
+    auto const [stop, stop_index] = _filter_ranges(frame);
+    if (stop)
     {
-      auto angle = (double)frame.angle_min;
-      for (size_t i = 1; i < frame.ranges.size(); i++)
-      {
-        angle += frame.angle_increment;
-        if ((-angle_max <= angle) && (angle <= angle_max))
-        {
-          if (_range_start == 0)
-            _range_start = i;
-
-          _angles.push_back(angle);
-        }
-      }
-      _filtered.resize(_angles.size());
-    }
-
-    auto const* ranges = &frame.ranges[_range_start];
-    std::copy(&ranges[0], &ranges[_angles.size()], _filtered.begin());
-
-    size_t stop_counter = 0;
-    for (int i = 0; i < (int)_angles.size(); i++)
-    {
-      if (
-        (ranges[i] < 0.5) ||
-        ((abs(_angles[i]) < (5.0 * deg_to_rad)) && (ranges[i] <= 1.0)))
-      {
-        printf("angle: %d, range: %.2f\n", _steering_angle, ranges[i]);
-        return CtlMsg{0, _steering_angle};
-      }
-
-      auto const r = std::min<float>(ranges[i], 10.0);
-      auto const t = atan2(safety_radius, r);
-      auto const d_idx = (int)std::ceil(t / frame.angle_increment);
-      auto const lower = std::max(0, i - d_idx);
-      auto const upper = std::min((int)_angles.size(), i + d_idx);
-      auto const local_min = *std::min_element(&ranges[lower], &ranges[upper]);
-      for (int j = lower; j < upper; j++)
-        _filtered[j] = std::min(_filtered[j], local_min);
+      printf(
+        "angle: %d, range: %.2f (stop)\n",
+        _steering_angle,
+        frame.ranges[stop_range]);
+      return CtlMsg{0, _steering_angle};
     }
 
     auto const path =
@@ -95,6 +70,57 @@ public:
       _filtered[path]);
 
     return CtlMsg{6, _steering_angle};
+  }
+
+private:
+  /// Initialize member variables that stay constant after the first frame
+  /// update.
+  void _init_buffers(sensor_msgs::LaserScan const& frame)
+  {
+    auto angle = (double)frame.angle_min;
+    for (size_t i = 1; i < frame.ranges.size(); i++)
+    {
+      angle += frame.angle_increment;
+      if ((-angle_max <= angle) && (angle <= angle_max))
+      {
+        if (_range_start == 0)
+          _range_start = i;
+
+        _angles.push_back(angle);
+      }
+    }
+    _filtered.resize(_angles.size());
+  }
+
+  /// Filter the data frame ranges. The first element of the return value is
+  /// true if the vehicle should stop, and false otherwise. If the first element
+  /// is true. Then the second element is the index at which the filtering
+  /// algorithm determined it should stop.
+  std::pair<bool, size_t> _filter_ranges(sensor_msgs::LaserScan const& frame)
+  {
+    auto const* ranges = &frame.ranges[_range_start];
+    std::copy(&ranges[0], &ranges[_angles.size()], _filtered.begin());
+    size_t stop_counter = 0;
+
+    for (size_t i = 0; i < _angles.size(); i++)
+    {
+      auto const obstacle_around = ranges[i] <= stop_threshold_ahead;
+      auto const obstacle_ahead = (abs(_angles[i]) < (5.0 * deg_to_rad)) &&
+        (ranges[i] <= stop_threshold_ahead);
+      if (obstacle_around || obstacle_ahead)
+        return {true, _range_start + i};
+
+      auto const r = std::min<float>(ranges[i], 10.0);
+      auto const t = std::atan2(safety_radius, r);
+      auto const d_idx = (int)std::ceil(t / angle_increment);
+      auto const lower = (size_t)std::max(0, (int)i - d_idx);
+      auto const upper = (size_t)std::min((int)_angles.size(), (int)i + d_idx);
+      auto const local_min = *std::min_element(&ranges[lower], &ranges[upper]);
+      for (size_t j = lower; j < upper; j++)
+        _filtered[j] = std::min(_filtered[j], local_min);
+    }
+
+    return {false, -1};
   }
 };
 
